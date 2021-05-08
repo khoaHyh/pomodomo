7df89be477fd9d4a88482daf936a9de0dab2a75c
khoaHyh <khoahuynh.toronto@gmail.com>
2021-05-08 19:40:13 -0400

    Update Interval size temporarily for screenshot

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Flex,
-  theme,
+import { ChakraProvider, Flex, theme } from '@chakra-ui/react';
+import { Navbar } from './components/Navbar';
+import Pomodoro from './components/Pomodoro';
 
-} from '@chakra-ui/react';
-import { Navbar } from "./components/Navbar"
-import Pomodoro from "./components/Pomodoro";
-
-
-
-function App() {
+const App = () => {
   return (
-    <ChakraProvider theme={theme} >
-      <Flex direction='column' align='center' justify='center'>
+    <ChakraProvider theme={theme}>
+      <Flex direction="column" align="center" justify="center">
         <Navbar />
-        <Flex direction='column' align='center' justify='center' w='100%' h='94vh' bg='red.400'>
+        <Flex
+          direction="column"
+          align="center"
+          justify="center"
+          w="100%"
+          h="94vh"
+          bg="red.400"
+        >
           <Pomodoro />
         </Flex>
       </Flex>
     </ChakraProvider>
   );
-}
+};
 
 export default App;
