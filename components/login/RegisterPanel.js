7028ad90ae6551e0d99902e28c16fa526f794add
Andrew Huynh <andrew.huynh17@gmail.com>
2021-05-10 21:15:23 -0400

    first and last name are required

--- components/login/RegisterPanel.js
+++ components/login/RegisterPanel.js
@@ -4,14 +4,21 @@ import {
   FormLabel,
 } from '@chakra-ui/form-control';
 import { Input } from '@chakra-ui/input';
+import { Stack } from '@chakra-ui/layout';
 
 export const RegisterPanel = () => {
   return (
     <>
-    <FormControl id="fullName">
-        <FormLabel>Full Name</FormLabel>
-        <Input type="text" />
-      </FormControl>
+    <Stack isInline>
+      <FormControl id="firstName">
+          <FormLabel>First Name</FormLabel>
+          <Input type="text" />
+        </FormControl>
+        <FormControl id="lastName">
+          <FormLabel>Last Name</FormLabel>
+          <Input type="text" />
+        </FormControl>
+    </Stack>
       <FormControl id="email">
         <FormLabel>Email</FormLabel>
         <Input type="email" />
