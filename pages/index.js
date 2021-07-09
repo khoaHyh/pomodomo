4c98b8ed3faeff2cd00643382cd6d85a79b9c232
Andrew Huynh <andrew.huynh17@gmail.com>
2021-07-08 23:07:16 -0400

    onmountcomponent set local login session to false

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import { Flex } from '@chakra-ui/react';
+import { useEffect } from 'react';
 import { Navbar } from '../components/Navbar';
 import Pomodoro from '../components/Pomodoro';
 
 const Index = () => {
+  useEffect(() => {
+    window.localStorage.setItem('isLoggedIn', false);
+  }, []);
+
   return (
     <Flex direction="column" align="center" justify="center">
       <Navbar />
