78aa6782255a55d82bcdecd50d6d1cd9cf5ceb89
Andrew Huynh <andrew.huynh17@gmail.com>
2021-07-16 20:00:59 -0400

    add in user profile data which is accesible when user logs in and got rid of console logs

--- components/userProfile/profileUtils.js
+++ components/userProfile/profileUtils.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
+
 const getUsername = () => {
   return window.localStorage.getItem('user');
 };
@@ -10,10 +11,8 @@ const getUserData = async () => {
     const res = await axios.get(
       `${process.env.NEXT_PUBLIC_API_URL}${'/profile'}`
     );
-    // console.log(res);
     return res;
   } catch (error) {
-    // console.log(error);
     return error;
   }
 };
