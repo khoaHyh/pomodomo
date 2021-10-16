caa9deb67a54fa6aa71a3d10e45690a4d2458f82
wAndrewx <andrew.huynh17@gmail.com>
2021-10-15 21:39:43 -0400

    fix time counting down too fast

--- components/Pomodoro.js
+++ components/Pomodoro.js
@@ -21,7 +21,7 @@ const Pomodoro = () => {
     if (isPlaying && timerPointer > 0) {
       timer = setInterval(() => {
         setTimerPointer((time = 1) => time - 1);
-      }, 1);
+      }, 1000);
     } else if (!timerPointer) {
       extra = setInterval(() => {
         handleSwitch(!sessionType);
